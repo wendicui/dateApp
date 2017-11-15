fb570ec78ef5a7d7e07953fd34d34246f3c628dc
Wendi Cui <wcui@igrace.com>
2017-11-15 12:09:11 -0500

    inital algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,38 @@
 var people = require("../data/friends");
 
 module.exports = function(app){
-
+//return full list of people
 	app.get('/api/friends', function(req,res){
 		res.json(people)
 	})
 
+//return the best match
 	app.post('.api/friends', function(req,res){
-		var newPerson = req.body;
 
+		var newPerson = req.body;
+		var newScore = newPerson.scores;
+		var bestMatch;
+		var score = 80 ;
+//loop through all person in people array
+		for (var i = 0; i < people.length; i++) {
+			var exstScore = people[i].scores;
+			var diff = 0;
+		//calculate the differnece of all questions
+			for (var j = 0; j < exst.length; i++) {
+				diff += Math.abs(exst[i] - newScore[j])
+			}
+		//check if it is a better choice
+			if (diff < score) {
+				bestMatch = people[i];
+				score = diff
+			}
 		
+		}
+
+
+		res.json(bestMatch);
+
+		people.push(newPerson);
 	})
 
 }
